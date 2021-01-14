147e7c5ab9265479e92197f94714901d4d05e403
Justyna Kolenda <justynakolenda7@gmail.com>
2021-01-14 15:37:09 +0100

    add button

--- src/screeens/ListScreen.tsx
+++ src/screeens/ListScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Dimensions, SafeAreaView, Text, TouchableOpacity,  } from 'react-native'
+import { Dimensions, SafeAreaView, Text,  } from 'react-native'
 import SymbolScreen from './SymbolScreen'
 import styled from 'styled-components/native'
 import CartScreen from './CartScreen'
@@ -10,9 +10,9 @@ const ListScreen = ()=> {
         <SafeAreaView style={{backgroundColor:'#000000'}}>
             <SymbolScreen/>
             <CartScreen/>
-            {/* <TouchableOpacity>
-                <Text>Kliknij</Text>
-            </TouchableOpacity> */}
+            <Button>
+                <ButtonText>+</ButtonText>
+            </Button>
         </SafeAreaView>
         </GroupView>
     )
@@ -22,5 +22,24 @@ const {width, height} = Dimensions.get('window')
 const GroupView = styled.View({
     backgroundColor:'#000000',
     height: height
+})
+
+const Button = styled.TouchableOpacity({
+    position:'absolute',
+    backgroundColor:'#136d94',
+    bottom:0,
+    right:10,
+    borderWidth: 2,
+    borderColor: '#ffffff',
+    borderRadius: 100,
+    width: 100,
+    height: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+})
+
+const ButtonText = styled.Text({
+    color: '#ffffff',
+    fontSize:20
 })
 export default ListScreen
